21c6323f2f94828f4957aa8f1e68c2f97b880651
EduardVardikyan <97230666+EduardVardikyan@users.noreply.github.com>
2025-10-15 16:44:03 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,7 @@
+from app import app
 
+def test_index():
+    client = app.test_client()
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.get_json() == {"message": "Hello CI/CD!"}
